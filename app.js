6de5bd6b4efd5438c13379d546a3443ae98b4217
Hack Reactor Student <student@hackreactor.com>
2017-06-02 13:25:19 -0700

    completed React components exercise

--- app.js
+++ app.js
@@ -9,9 +9,43 @@ var Kale = () => (
     Kale
   </div>
 );
-var GroceryListItem = (props) => (
-    <li>{props.listitem}</li>
-);
+// var GroceryListItem = (props) => (
+//     <li>{props.listitem}</li>
+// );
+class GroceryListItem extends React.Component {
+
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : ''
+    };
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+      <li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.listitem}</li>
+    );
+
+  }
+
+}
 var GroceryList = (props) => {
 
   var mapElem =props.todos.map((item) => {
